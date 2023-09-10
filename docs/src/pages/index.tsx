de31b901f363f87aab79603329f0e26ebf757b0c
Atul Gupta <iamatulmgupta@gmail.com>
2023-09-10 13:40:50 -0700

    Update index.tsx

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -11,9 +11,7 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      <div className="container">       
         {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
